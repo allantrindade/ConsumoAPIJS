6502ae7823d3a8dabac34a4e84322e5f17e56d76
Allan Abreu <allan-10@live.com>
2020-11-25 20:47:18 -0300

    Correção Bugs de Cache do Fetch

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// URL usada como base para consumo via API
 const URL_BASE = 'https://wllsistemas.com.br/api/v3/cliente/'
 
 
@@ -10,11 +11,12 @@ const Limpar = () => {
     ErroNome.innerHTML = ''
     ErroEmail.innerHTML = ''
     ErroTipo.innerHTML = ''
+    acao.value = 'incluir'
 
 }
 //Função para Carregar Todos as Pessoas Através de uma API
 const Carregar = () => {
-    fetch(URL_BASE)
+    fetch(URL_BASE, { cache: 'no-store' })
         .then(response => response.json())
         .then(json => {
             let Linha = ''
@@ -25,8 +27,10 @@ const Carregar = () => {
                     '<td>' + pessoa.EMAIL + '</td>' +
                     '<td>' + pessoa.TIPO + '</td>' +
                     '<td>' +
-                    '<button type="button" id="btn-editar-' + pessoa.ID + '" class="btn btn-primary">Editar</button>' + ' ' +
-                    '<button type="button" id="btn-excluir-' + pessoa.ID + '" class="btn btn-danger">Excluir</button>' +
+                    '<button type="button" id="btn-editar-' + pessoa.ID + 
+                    '" class="btn btn-info btn-sm">Editar</button>' + ' ' +
+                    '<button type="button" id="btn-excluir-' + pessoa.ID + 
+                    '" class="btn btn-danger btn-sm">Excluir</button>' +
                     '</td>' +
                     '</tr>'
             })                             
@@ -84,44 +88,12 @@ const Deletar = i_d => {
 //Função Para Gravar Dados Alterado
 const GravarDados = () => {
     acao.value == 'incluir' ? Gravar() : Alterar()
-    acao.value = 'editar'
 }
 
 
 //Função Para Gravar Uma Pessoa
 const Gravar = () => {
-    if (nome.value.length < 1){
-        ErroNome.innerHTML = 'Por favor, preencha o campo nome<BR><BR>'
-        ErroNome.style.color = 'red'
-        ErroEmail.innerHTML = ''
-        ErroTipo.innerHTML = ''
-        nome.focus()
-        return false       
-    }
-    else if (email.value.length < 1){
-        ErroNome.innerHTML = ''
-        ErroEmail.innerHTML = 'Por favor, preencha o campo email<BR><BR>'
-        ErroEmail.style.color = 'red'
-        ErroTipo.innerHTML = ''
-        email.focus()
-        return false
-    }
-    else if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1 ){
-        ErroNome.innerHTML = ''
-        ErroEmail.innerHTML = 'Por favor, informe um E-MAIL válido!<BR><BR>'
-        ErroEmail.style.color = 'red'
-        ErroTipo.innerHTML = ''
-        email.focus()
-        return false        
-    }
-    else if (tipo.value.length < 1){
-        ErroNome.innerHTML = ''
-        ErroEmail.innerHTML = ''
-        ErroTipo.innerHTML = 'Por favor, selecione o tipo<BR><BR>'
-        ErroTipo.style.color = 'red'
-        tipo.focus()
-        return false       
-    } else {
+     if (ValidarCampos()){
         fetch(URL_BASE, {
             method: 'POST',
             body: 'nome=' + nome.value + '&email=' + email.value + '&tipo=' + tipo.value,
@@ -143,6 +115,7 @@ const Gravar = () => {
 
 //Função Para Alterar um Registro
 const Alterar = () => {
+    if (ValidarCampos()){
     fetch(URL_BASE, {
         method: 'PUT',
         body: 'id=' + id.value + '&nome=' + nome.value + '&email=' + email.value + '&tipo=' + tipo.value,
@@ -150,21 +123,21 @@ const Alterar = () => {
     })
         .then(response => {
             if (response.status == 202) {
-                CarregarReg()
-                Limpar()
                 Carregar()
+                Limpar()
             }
             return response.json()
         })
         .then(json => {
             swal("Bom trabalho!",json.mensagem, "success")
         })
+    }
 }
 
 
 //Função Para Recarregar as Informações de uma Pessoa no Input para Edição de Dados
 const CarregarReg = i_d => {
-    fetch(URL_BASE + i_d)
+    fetch(URL_BASE + i_d, { cache: 'no-store' })
         .then(response => response.json())
         .then(pessoa => {
             id.value = pessoa[0].ID
@@ -176,6 +149,45 @@ const CarregarReg = i_d => {
  }
 
 
+ // Função para validar se os campos não estão vazios, caso esteja retorna false caso contrário retorna true
+ const ValidarCampos = () => {
+    if (nome.value.length < 1){
+        ErroNome.innerHTML = 'Por favor, preencha o campo nome!<BR><BR>'
+        ErroNome.style.color = 'red'
+        ErroEmail.innerHTML = ''
+        ErroTipo.innerHTML = ''
+        nome.focus()
+        return false       
+    }
+    else if (email.value.length < 1){
+        ErroNome.innerHTML = ''
+        ErroEmail.innerHTML = 'Por favor, preencha o campo email!<BR><BR>'
+        ErroEmail.style.color = 'red'
+        ErroTipo.innerHTML = ''
+        email.focus()
+        return false
+    }
+    else if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1 ){
+        ErroNome.innerHTML = ''
+        ErroEmail.innerHTML = 'Por favor, informe um E-MAIL válido!<BR><BR>'
+        ErroEmail.style.color = 'red'
+        ErroTipo.innerHTML = ''
+        email.focus()
+        return false        
+    }
+    else if (tipo.value.length < 1){
+        ErroNome.innerHTML = ''
+        ErroEmail.innerHTML = ''
+        ErroTipo.innerHTML = 'Por favor, selecione o tipo!<BR><BR>'
+        ErroTipo.style.color = 'red'
+        tipo.focus()
+        return false       
+    } else {
+        return true
+    }
+ }
+
+
 //Evento Botão Gravar
 btnGravar.addEventListener('click', GravarDados)
 
@@ -184,6 +196,5 @@ btnCarregar.addEventListener('click', Carregar)
 
 //Evento Botão Limpar
 btnLimpar.addEventListener('click', Limpar)
-
 Carregar()
 Limpar()
