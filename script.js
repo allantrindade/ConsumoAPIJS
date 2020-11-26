13a1902942fc4359e6e7c3dd95221de9267b3686
Allan Abreu <allan-10@live.com>
2020-11-26 18:00:18 -0300

    Correçao de bugs

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // URL usada como base para consumo via API
 const URL_BASE = 'https://wllsistemas.com.br/api/v3/cliente/'
 
-
 //Função Para Limpar Todos os Campos
 const Limpar = () => {
     id.value = ''
@@ -12,11 +11,10 @@ const Limpar = () => {
     ErroEmail.innerHTML = ''
     ErroTipo.innerHTML = ''
     acao.value = 'incluir'
-
 }
 //Função para Carregar Todos as Pessoas Através de uma API
 const Carregar = () => {
-    fetch(URL_BASE, { cache: 'no-store' })
+    fetch(URL_BASE, { cache: 'reload' })
         .then(response => response.json())
         .then(json => {
             let Linha = ''
@@ -52,7 +50,6 @@ const Carregar = () => {
         })
 }
 
-
 //Função Para Deletar uma Pessoa através do ID
 const Deletar = i_d => {
     swal({
@@ -65,7 +62,8 @@ const Deletar = i_d => {
     .then((willDelete) => {
         if (willDelete) {
 	        fetch(URL_BASE + i_d, {
-            method:'DELETE'
+            method:'DELETE',
+            cache:'reload'
         })
         .then(response => {
             response.status == 202 ? document.getElementById('linha' + i_d).remove() : false
@@ -84,17 +82,16 @@ const Deletar = i_d => {
 
 }
 
-
 //Função Para Gravar Dados Alterado
 const GravarDados = () => {
     acao.value == 'incluir' ? Gravar() : Alterar()
 }
 
-
 //Função Para Gravar Uma Pessoa
 const Gravar = () => {
      if (ValidarCampos()){
         fetch(URL_BASE, {
+            cache: 'reload',
             method: 'POST',
             body: 'nome=' + nome.value + '&email=' + email.value + '&tipo=' + tipo.value,
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' }    
@@ -112,11 +109,11 @@ const Gravar = () => {
     }
 }
 
-
 //Função Para Alterar um Registro
 const Alterar = () => {
     if (ValidarCampos()){
     fetch(URL_BASE, {
+        cache: 'reload',
         method: 'PUT',
         body: 'id=' + id.value + '&nome=' + nome.value + '&email=' + email.value + '&tipo=' + tipo.value,
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
@@ -134,10 +131,9 @@ const Alterar = () => {
     }
 }
 
-
 //Função Para Recarregar as Informações de uma Pessoa no Input para Edição de Dados
 const CarregarReg = i_d => {
-    fetch(URL_BASE + i_d, { cache: 'no-store' })
+    fetch(URL_BASE + i_d, { cache: 'reload' })
         .then(response => response.json())
         .then(pessoa => {
             id.value = pessoa[0].ID
@@ -148,7 +144,6 @@ const CarregarReg = i_d => {
         })
  }
 
-
  // Função para validar se os campos não estão vazios, caso esteja retorna false caso contrário retorna true
  const ValidarCampos = () => {
     if (nome.value.length < 1){
@@ -158,35 +153,29 @@ const CarregarReg = i_d => {
         ErroTipo.innerHTML = ''
         nome.focus()
         return false       
-    }
-    else if (email.value.length < 1){
+    } else if (email.value.length < 1){
         ErroNome.innerHTML = ''
         ErroEmail.innerHTML = 'Por favor, preencha o campo email!<BR><BR>'
         ErroEmail.style.color = 'red'
         ErroTipo.innerHTML = ''
         email.focus()
         return false
-    }
-    else if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1 ){
+    } else if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1 ){
         ErroNome.innerHTML = ''
         ErroEmail.innerHTML = 'Por favor, informe um E-MAIL válido!<BR><BR>'
         ErroEmail.style.color = 'red'
         ErroTipo.innerHTML = ''
         email.focus()
         return false        
-    }
-    else if (tipo.value.length < 1){
+    } else if (tipo.value.length < 1){
         ErroNome.innerHTML = ''
         ErroEmail.innerHTML = ''
         ErroTipo.innerHTML = 'Por favor, selecione o tipo!<BR><BR>'
         ErroTipo.style.color = 'red'
         tipo.focus()
         return false       
-    } else {
-        return true
-    }
+    } else { return true }
  }
-
 
 //Evento Botão Gravar
 btnGravar.addEventListener('click', GravarDados)
